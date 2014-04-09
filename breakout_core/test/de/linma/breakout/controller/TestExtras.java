319bfefa353cbdfb10c3ba235322e441890b4320
OSI-Califronia <OSI-@gmx.net>
2014-04-09 10:52:03 +0200

    add old Tests to Project

---
+++ breakout_core/test/de/linma/breakout/controller/TestExtras.java
@@ -0,0 +1,70 @@
+package de.linma.breakout.controller;
+
+import junit.framework.TestCase;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import de.linma.breakout.communication.GAME_STATE;
+import de.linma.breakout.communication.MENU_ITEM;
+import de.linma.breakout.controller.IGameController.PLAYER_INPUT;
+
+public class TestExtras extends TestCase {
+
+	private GameController controller;
+
+	@Before
+	public void setUp() throws Exception {
+		System.out.println("setUp()\n");
+
+		// create controller
+		controller = new GameController(this.getClass().getName());
+		controller.clearGrid();
+
+		controller.initialize();
+	}
+
+	@Test
+	public void testLevelEditing() {
+		controller.processMenuInput(MENU_ITEM.MNU_LEVEL_EDITOR);
+		assertTrue(controller.getCreativeMode());
+
+		// controller knows at least one type of brick
+		assertFalse(controller.getBrickClasses().isEmpty());
+
+
+	}
+
+
+	@Test
+	public void testMenuInputs() {
+
+		assertTrue(controller.getState() == GAME_STATE.MENU_MAIN);
+
+		controller.processMenuInput(MENU_ITEM.MNU_NEW_GAME);
+		assertTrue(controller.getState() == GAME_STATE.RUNNING);
+
+		controller.processGameInput(PLAYER_INPUT.PAUSE);
+		assertTrue(controller.getState() == GAME_STATE.PAUSED);
+
+		controller.processMenuInput(MENU_ITEM.MNU_CONTINUE);
+		assertTrue(controller.getState() == GAME_STATE.RUNNING);
+
+		controller.processGameInput(PLAYER_INPUT.PAUSE);
+		controller.processMenuInput(MENU_ITEM.MNU_BACK_MAIN_MENU);
+		assertTrue(controller.getState() == GAME_STATE.MENU_MAIN);
+
+		controller.processMenuInput(MENU_ITEM.MNU_END);
+		assertTrue(controller.getState() == GAME_STATE.KILLED);
+	}
+
+
+	@Override
+	public void tearDown() {
+		System.out.println("tearDown()\n");
+
+		controller.processMenuInput(MENU_ITEM.MNU_END);
+		controller = null;	
+	}
+
+}
---
+++ breakout_core/test/de/linma/breakout/controller/TestUITextView.java
@@ -0,0 +1,49 @@
+package de.linma.breakout.controller;
+
+import junit.framework.TestCase;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import de.linma.breakout.communication.MENU_ITEM;
+import de.linma.breakout.view.tui.UITextView;
+
+public class TestUITextView extends TestCase {
+
+	private GameController controller;
+
+
+	@Before
+	public void setUp() throws Exception {
+		System.out.println("setUp()\n");
+
+		// create controller
+		controller = new GameController(this.getClass().getName());
+		controller.clearGrid();
+
+
+
+		controller.initialize();
+	}
+
+
+	@Test
+	public void testConsoleInput() throws InterruptedException {
+		// create view
+		UITextView view;
+		view = new UITextView("0\na\nd\np\n3\np\n4\n5\n");
+		view.setController(controller);		
+		controller.addObserver(view);
+
+		Thread.sleep(2000); // wait 2 sek to let console work
+	}
+
+	@Override
+	public void tearDown() {
+		System.out.println("tearDown()\n");
+
+		controller.processMenuInput(MENU_ITEM.MNU_END);
+		controller = null;	
+	}
+
+}
